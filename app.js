234a408414a68ebacebc55e159a317c0c2fc044d
vamsi8979 <vamsi8979@gmail.com>
2018-10-10 11:34:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//for express js
+var express = require("express");
+var app     = express();
+app.set('view engine' , 'ejs');
+app.get( "/" , function(req,res){
+    res.render("home");
+});
+app.listen(process.env.PORT,process.env.IP);
+// app.listen("3001","localhost",function(){
+//     console.log("localhost:3001");
+// });
