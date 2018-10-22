1ed341f94c2bd504be8f3a7c55008d23786f7632
vamsi8979 <vamsi8979@gmail.com>
2018-10-22 13:20:19 +0530

    added contact page haha

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.set('view engine' , 'ejs');
 app.get( "/" , function(req,res){
     res.render("home");
 });
+app.get( "/contact" , function(req,res){
+    res.render("contact");
+});
 app.listen(process.env.PORT,process.env.IP);
 // app.listen("3001","localhost",function(){
 //     console.log("localhost:3001");
