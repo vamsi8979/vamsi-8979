e19f1ca7dc6ce59f3caf3a4d9f238b971c820141
vamsi8979 <vamsi8979@gmail.com>
2018-11-02 15:03:02 +0530

    js jquery fontawesome and css added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //for express js
 var express = require("express");
 var app     = express();
+app.use( express.static(   "public" ) );
 app.set('view engine' , 'ejs');
 app.get( "/" , function(req,res){
     res.render("home");
