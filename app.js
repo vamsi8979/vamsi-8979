baac2846c383b272403f282e146f5c74884f8a45
vamsi8979 <vamsi8979.pro@gmail.com>
2018-11-09 17:24:33 +0530

    ip address test

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 //for express js
 var express = require("express");
 var app     = express();
+const expressip = require('express-ip');
 app.use( express.static(   "public" ) );
 app.set('view engine' , 'ejs');
+app.use(expressip().getIpInfoMiddleware);
 app.get( "/" , function(req,res){
     res.render("home");
 });
 app.get( "/contact" , function(req,res){
     res.render("contact");
 });
-app.listen(process.env.PORT,process.env.IP);
-// app.listen("3001","localhost",function(){
-//     console.log("localhost:3001");
-// });
+app.get("/ip",function(req,res){
+  const ipInfo = req.ipInfo;
+  var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
+  res.send(message);
+});
+// app.listen(process.env.PORT,process.env.IP);
+app.listen("3001","localhost",function(){
+    console.log("localhost:3001");
+});
