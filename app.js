c8404bfdd12f596d6ecaf4e9b64790948dbe8fb5
vamsi8979 <vamsi8979.pro@gmail.com>
2018-11-19 12:48:50 +0530

    added 404 route

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get( "/" , function(req,res){
 app.get( "/contact" , function(req,res){
     res.render("contact");
 });
+//other page not found handler
+app.get('*', function(req, res, next){
+	res.status(404);
+	res.send('<h1>YOU ARE LOOKING FOR 404 <span style="color : red ; "> NOT FOUND </span></h1>');
+});
 app.listen(process.env.PORT,process.env.IP);
 // app.listen("3001","localhost",function(){
 //     console.log("localhost:3001");
