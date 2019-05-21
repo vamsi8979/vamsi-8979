e644a78a3b48de90787739b1465297e8c13755a4
vamsi_iesoft <vamsi@iesoftek.com>
2019-05-21 14:47:46 +0530

    changed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get( "/contact" , function(req,res){
 //other page not found handler
 app.get('*', function(req, res, next){
 	res.status(404);
-	res.send('<h1>THE PAGE YOU ARE LOOKING FOR  <span style="color : blue ; "> 404 NOT FOUND </span></h1>');
+	res.send('<h1>THE PAGE YOU ARE LOOKING FOR  <span style="color : blue ; "> 404 NOT FOUND </span></h1><h2> SMall H2 tag is here </h2>');
 });
 app.listen(process.env.PORT,process.env.IP);
 // app.listen("3001","localhost",function(){
